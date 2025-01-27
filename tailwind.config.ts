d39252ecb27af7cb7eb78ebfccb9dedb1a7d3f90
non_hana <1209220829@qq.com>
2025-01-27 22:10:30 +0800

    feat: 配置主题色

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,24 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'padua': {  
+          DEFAULT: '#A8E6CF',
+            50: '#FBFEFD',  
+            100: '#EFFAF6', 
+            200: '#D8F4E9',  
+            300: '#C0EDDC',
+            400: '#A8E6CF', 
+            500: '#80DBB9', 
+            600: '#59CFA3', 
+            700: '#37BE8C',
+            800: '#2B976F', 
+            900: '#206F52', 
+            950: '#1A5B43'
+        },
+      }
+    }
   },
 
   plugins: [typography, forms, containerQueries]
