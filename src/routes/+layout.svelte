31084b7f8e4aae6762f531ef03d06435ab0f6922
non_hana <1209220829@qq.com>
2025-01-27 23:40:52 +0800

    feat: 新建页面

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,59 +1,19 @@
 <script lang='ts'>
-  import Header from './Header.svelte'
   import '../styles/index.scss'
 
   const { children } = $props()
 </script>
 
-<div class='app'>
-  <Header />
+<div class='layout'>
+  <header class='header'>
+    <h1>这是 header 的内容哦~</h1>
+  </header>
 
   <main>
     {@render children()}
   </main>
 
   <footer>
-    <p class='text-sm'>123</p>
-    <p class='font-bold'>
-      visit <a href='https://svelte.dev/docs/kit'>svelte.dev/docs/kit</a> to learn
-      about SvelteKit
-    </p>
+    <p>这是 footer 的内容哦~</p>
   </footer>
 </div>
-
-<style>
-  .app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
-
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    width: 100%;
-    max-width: 64rem;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 12px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0;
-    }
-  }
-</style>
