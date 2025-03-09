d101c7fbb7841127c0e97705a38d579919017db4
non_hana <1209220829@qq.com>
2025-03-10 00:54:11 +0800

    fix: 修复打包构建预渲染时找不到 env 环境变量的问题

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,9 +1,9 @@
-import { env } from '$env/dynamic/private'
+import { DATABASE_URL } from '$env/static/private'
 import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 import * as schema from './schema'
 
-if (!env.DATABASE_URL)
+if (!DATABASE_URL)
   throw new Error('DATABASE_URL is not set')
-const client = createClient({ url: env.DATABASE_URL })
+const client = createClient({ url: DATABASE_URL })
 export const db = drizzle(client, { schema })
