c61a184846f98307465f9f50e013eab8114cb077
non_hana <1209220829@qq.com>
2025-03-10 22:29:38 +0800

    chore: Update Drizzle config for Cloudflare D1 database credentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,12 +5,13 @@ if (!process.env.DATABASE_URL)
   throw new Error('DATABASE_URL is not set')
 
 export default defineConfig({
+  out: './drizzle',
   schema: './src/lib/server/db/schema.ts',
-
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+    token: process.env.CLOUDFLARE_D1_TOKEN!,
   },
-
   verbose: true,
   strict: true,
   dialect: 'sqlite',
