2b86a87497d4404c8ce8b47f7d8545bbfd7f8bb8
non_hana <1209220829@qq.com>
2025-03-11 11:12:34 +0800

    feat: 配置 netlify edge runtime

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,9 +8,7 @@ export default defineConfig({
   out: './drizzle',
   schema: './src/lib/server/db/schema.ts',
   dbCredentials: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-    token: process.env.CLOUDFLARE_D1_TOKEN!,
+    url: process.env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
