8515b9a5e74d9a45133a9aa409226df1e8314c1d
non_hana <1209220829@qq.com>
2025-03-11 13:25:58 +0800

    feat: 完善 turso sqlite drizzle 配置

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,17 @@
 import process from 'node:process'
 import { defineConfig } from 'drizzle-kit'
 
-if (!process.env.DATABASE_URL)
-  throw new Error('DATABASE_URL is not set')
+if (!process.env.TURSO_DATABASE_URL || !process.env.TURSO_AUTH_TOKEN)
+  throw new Error('TURSO_DATABASE_URL or TURSO_AUTH_TOKEN is not set')
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/lib/server/db/schema.ts',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN,
   },
   verbose: true,
   strict: true,
-  dialect: 'sqlite',
+  dialect: 'turso',
 })
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,9 +1,13 @@
-import { DATABASE_URL } from '$env/static/private'
+import { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from '$env/static/private'
 import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 import * as schema from './schema'
 
-if (!DATABASE_URL)
-  throw new Error('DATABASE_URL is not set')
-const client = createClient({ url: DATABASE_URL })
+if (!TURSO_AUTH_TOKEN || !TURSO_DATABASE_URL)
+  throw new Error('TURSO_DATABASE_URL or TURSO_AUTH_TOKEN is not set')
+
+const client = createClient({
+  url: TURSO_DATABASE_URL,
+  authToken: TURSO_AUTH_TOKEN,
+})
 export const db = drizzle(client, { schema })
