41767ab838c99a77e374df4f31d73349032659bc
non_hana <1209220829@qq.com>
2025-03-17 00:27:23 +0800

    fix: 修复 songs 页面找不到路由的问题

--- src/routes/songs/[page]/+page.server.ts
+++ src/routes/songs/[page]/+page.server.ts
@@ -1,5 +1,11 @@
 import type { PageServerLoad } from './$types'
-import { getSongList } from '$lib/server/services/songService'
+import { getSongCount, getSongList } from '$lib/server/services/songService'
+
+export const prerender = true
+export async function entries() {
+  const songCount = await getSongCount()
+  return Array.from({ length: Math.ceil(songCount / 50) }, (_, i) => ({ page: String(i + 1) }))
+}
 
 export const load: PageServerLoad = async ({ params }) => {
   const page = Number(params.page)
