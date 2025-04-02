2426b861dc7eb42c7bb63866041baa24e97df2a3
non_hana <1209220829@qq.com>
2025-04-02 23:44:04 +0800

    feat: 完善歌单详情页响应式 UI

--- src/lib/components/playlists/Detail.svelte
+++ src/lib/components/playlists/Detail.svelte
@@ -8,7 +8,7 @@
   import LazyImage from '$lib/components/hana/LazyImage.svelte'
   import useMessage from '$lib/hooks/useMessage'
   import { setNowPlaying, setPlaylist, setSongLoading, songLoading } from '$lib/stores'
-  import { Ellipsis, Loader, Play } from 'lucide-svelte'
+  import { Ellipsis, Loader, Play, Plus } from 'lucide-svelte'
 
   const { callHanaMessage } = useMessage()
 
@@ -66,17 +66,17 @@
   }
 </script>
 
-<div class='flex gap-8'>
+<div class='flex gap-4 md:gap-8'>
   <LazyImage
     src={playlist.cover ?? ''}
     alt={`歌单 ${playlist.name} 的封面`}
-    class='aspect-square w-32 shrink-0 rounded-2xl md:w-48'
+    class='aspect-square size-32 shrink-0 rounded-2xl md:size-48'
   />
   <div class='flex flex-col justify-between'>
-    <h2 class='text-2xl font-semibold'>{playlist.name}</h2>
-    <p class='max-h-[76px] overflow-y-scroll break-words text-wrap text-neutral scrollbar-none'>{playlist.description ?? '暂无描述'}</p>
+    <h2 class='font-semibold md:text-2xl'>{playlist.name}</h2>
     <p class='text-neutral'>{playlist.musicCount} 首歌曲</p>
-    <div class='flex space-x-5'>
+    <p class='line-clamp-2 break-words text-wrap text-xs text-neutral scrollbar-none md:line-clamp-none md:max-h-[76px] md:overflow-y-scroll md:text-sm'>{playlist.description ?? '暂无描述'}</p>
+    <div class='hidden md:flex space-x-5'>
       <Button variant='accent' onclick={() => handleAddPlaylistSongs(true)} disabled={$songLoading}>
         <span class='flex items-center gap-2'>
           {#if $songLoading}
@@ -102,4 +102,21 @@
       </Dropdown>
     </div>
   </div>
+</div>
+
+<div class='w-full flex justify-between space-x-5 md:hidden'>
+  <Button class='flex-1' variant='accent' onclick={() => handleAddPlaylistSongs(true)} disabled={$songLoading}>
+    <span class='mx-auto inline-block w-fit flex items-center gap-2'>
+      {#if $songLoading}
+        <Loader size={16} class='animate-spin' />
+      {:else}
+        <Play size={16} />
+      {/if} 播放全部
+    </span>
+  </Button>
+  <Button class='flex-1' variant='secondary' onclick={() => handleAddPlaylistSongs()}>
+    <span class='mx-auto inline-block w-fit flex items-center gap-2'>
+      <Plus /> 添加到播放列表
+    </span>
+  </Button>
 </div>
